37db985fafaea43e7be45f4bbda3c86c198951db
qisf <qisf@fun.tv>
2020-07-29 14:00:53 +0800

    a

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,21 +7,53 @@ module.exports = {
 	},
 	themeConfig: {
 		sidebarDepth: 3,
-		nav: [ // 导航栏配置
-			{
-				text: '博客',
-				link: '/blog/'
-			}
-		],
-		sidebar: {
-			'/blog/': [{
-				title: '文档',
-				collapsable: false,
-				children: [
-					'document/vuepress',
-				]
-			}]
-
-		}
+		nav: [
+      { text: '首页', link: '/' },
+      { text: '文档', link: '/guide/' },
+      { text: '链接', link: 'https://google.com' },
+    ],
+		sidebar: [
+      ['blog/','关于'],
+      // {
+      //   title: '技术',
+      //   collapsable: true,
+      //   children: [
+      //     ['/jishu/MarkDownGrammer','md语法'],
+      //     {
+      //       title: '技术1',
+      //       collapsable: true,
+      //       children: [
+      //         ['/jishu/MarkDownGrammer','目录1']
+      //       ]
+      //     }
+      //   ]
+      // },
+      {
+        title: 'iOS',
+        collapsable: true,
+        children: [
+          ['blog/iOS/0.ram','内存管理']
+        ]
+      },
+      {
+        title: '随笔',
+        collapsable: true,
+        children: [
+          ['blog/Other/Mark','Mark'],
+          ['blog/Other/MarkDownGrammer','md语法']
+        ]
+      },
+      // ['/page-a/f3','11']
+    ]
+		// sidebar: {
+		// 	'/blog/': [{
+		// 		title: '文档',
+		// 		collapsable: false,
+		// 		children: [
+		// 			'document/vuepress',
+		// 		]
+		// 	}]
+		//
+		// }
 	}
 };
