cd0d261b10205c541153fe4fc7d049d4c318d590
qisf <qisf@fun.tv>
2020-07-29 11:47:04 +0800

    a

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,6 @@
 module.exports = {
 	title: 'canaan',
 	description: 'The place is canaan',
-	head: [ // 注入到当前页面的 HTML <head> 中的标签
-		[
-			'link', {
-				rel: 'icon',
-				href: '/logo.jpg'
-			}
-
-		] // 增加一个自定义的 favicon(网页标签的图标)
-	],
 	base: '/test/', // 这是部署到github相关的配置
 	markdown: {
 		lineNumbers: true // 代码块显示行号
